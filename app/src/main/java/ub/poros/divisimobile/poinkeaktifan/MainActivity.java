93b7657a97a3d46a208ad8a37dd9349aede2d944
Nyamuk <asamsulfat13@gmail.com>
2018-09-11 21:40:44 +0700

    Add: sample HTTP GET with Fuel library

--- app/src/main/java/ub/poros/divisimobile/poinkeaktifan/MainActivity.java
+++ app/src/main/java/ub/poros/divisimobile/poinkeaktifan/MainActivity.java
@@ -1,7 +1,15 @@
 package ub.poros.divisimobile.poinkeaktifan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.github.kittinunf.fuel.Fuel;
+import com.github.kittinunf.fuel.core.FuelError;
+import com.github.kittinunf.fuel.core.Handler;
+import com.github.kittinunf.fuel.core.Request;
+import com.github.kittinunf.fuel.core.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testGet();
+        startActivity(new Intent(this, BikinPostActivity.class));
+    }
+
+    private void testGet() {
+        //get
+        Fuel.get("https://httpbin.org/get").responseString(new Handler<String>() {
+            @Override
+            public void failure(Request request, Response response, FuelError error) {
+                Log.d("fuel failed", "failure: ");
+            }
+
+            @Override
+            public void success(Request request, Response response, String data) {
+                Log.d("fuel success", response.toString());
+            }
+        });
     }
 }
