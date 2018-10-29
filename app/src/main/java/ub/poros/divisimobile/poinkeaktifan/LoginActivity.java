c1fb0b52c9a88fd4832dff03a43a7b38520314ff
nyamukTerbang <asamsulfat13@gmail.com>
2018-10-30 05:24:10 +0700

    Update init login view

--- app/src/main/java/ub/poros/divisimobile/poinkeaktifan/LoginActivity.java
+++ app/src/main/java/ub/poros/divisimobile/poinkeaktifan/LoginActivity.java
@@ -2,12 +2,25 @@ package ub.poros.divisimobile.poinkeaktifan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginActivity;
+    private EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView() {
+        loginActivity = (Button) findViewById(R.id.btn_login);
+        etUsername = (EditText) findViewById(R.id.et_username);
+        etPassword = (EditText) findViewById(R.id.et_password);
     }
 }
