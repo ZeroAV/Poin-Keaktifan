da9c0cc4db944437980b6d7d4775ed95a5e63497
nyamukTerbang <asamsulfat13@gmail.com>
2018-10-30 05:28:09 +0700

    Add intent to ListPostActivity when button clicked

--- app/src/main/java/ub/poros/divisimobile/poinkeaktifan/LoginActivity.java
+++ app/src/main/java/ub/poros/divisimobile/poinkeaktifan/LoginActivity.java
@@ -1,13 +1,16 @@
 package ub.poros.divisimobile.poinkeaktifan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button loginActivity;
+    private Button btnLogin;
     private EditText etUsername, etPassword;
 
     @Override
@@ -19,8 +22,19 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        loginActivity = (Button) findViewById(R.id.btn_login);
+        btnLogin = (Button) findViewById(R.id.btn_login);
         etUsername = (EditText) findViewById(R.id.et_username);
         etPassword = (EditText) findViewById(R.id.et_password);
+
+        btnLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                startActivity(new Intent(v.getContext(), ListPostActivity.class));
+                break;
+        }
     }
 }
